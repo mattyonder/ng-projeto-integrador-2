2b0af6543d4059cff4c51bb0eae96b4f1b4dbfc7
mattyonder <js.mateussantos05@gmail.com>
2025-05-12 23:15:46 -0300

    Adicionando mais informações na tela de home

--- src/app/modules/pages/pages.component.ts
+++ src/app/modules/pages/pages.component.ts
@@ -8,27 +8,31 @@ import { BaseComponent } from '../../../shared/utils/base.component';
   standalone: true,
   imports: [RouterOutlet, FontAwesomeModule],
   template: `
-    <main class="flex h-screen w-screen">
+    <main class="flex">
+      <!-- ASIDE FIXO -->
       <aside
-        class="flex flex-col bg-primary-2 border-primary-1 w-1/6 rounded-r-xl shadow-xl  py-3 px-3 text-white font-semibold"
+        class="fixed top-0 left-0 h-screen w-1/6 bg-primary-2 border-primary-1 rounded-r-xl shadow-xl py-3 px-3 text-white font-fixed z-50"
       >
-        <span class="text-xl"> TS ServiceHub </span>
+        <span class="text-xl block mb-6">TS ServiceHub</span>
 
-        <div class="flex flex-col mt-8 underline px-5 gap-4 ">
+        <div class="flex flex-col underline px-5 gap-4">
           <a class="text-link" routerLink="/home">Home</a>
           <a class="text-link">Abrir Chamado</a>
           <a class="text-link">Meu Chamados</a>
           <a class="text-link">Chamados em Aberto</a>
-          <a class="text-link">Abrir Chamado</a>
           <a class="text-link">Equipes</a>
           <a class="text-link">Dashboards</a>
-
           <a class="text-link">Categorias de Chamados</a>
           <a class="text-link">Instituição</a>
           <a class="text-link">Configurações</a>
         </div>
       </aside>
-      <router-outlet />
+
+      <div
+        class="ml-[16.6667%] w-[83.3333%] min-h-screen px-6 py-4 overflow-y-auto"
+      >
+        <router-outlet></router-outlet>
+      </div>
     </main>
   `,
   styles: '',
